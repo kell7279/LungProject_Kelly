9fc390dc0020ecc32268cff117953472cf84adff
Kelly Mosesso <kellymosesso@Kellys-MacBook-Pro-3.local>
2018-10-15 12:02:28 -0400

    added code for looking at expression before and after PCA adjustment

--- Analyses/Lung_noTCGA.R
+++ Analyses/Lung_noTCGA.R
@@ -1,5 +1,7 @@
 ### Lung Analyses Without Utilizing TCGA Data for Feature Selection ###
 
+library(pROC)
+
 dataDir <- "~/Dropbox (Partners HealthCare)/Lung CTC Project/Lung CTC Samples/LungProject_Kelly/Data"
 #Save figures here:
 figDir <- "~/Dropbox (Partners HealthCare)/Lung CTC Project/Lung CTC Samples/LungProject_Kelly/Output/Figures"
@@ -30,6 +32,7 @@ png(paste0(figDir,"/Lung_noTCGA.png"), width = 6, height = 4, units = 'in', res 
 plot.roc(adeno, rowMeans(noPCA_noTCGA_ttest$votes), col="black", asp=NA, legacy.axes=TRUE, main="Lung - No TCGA")
 lines.roc(adeno, rowMeans(PCA_noTCGA_ttest_3$votes), col="red")
 lines.roc(adeno, rowMeans(PCA_noTCGA_ttest_3$votes_adj), col="green")
+abline(v=0.9, col="purple")
 legend("bottomright",  title="Analysis",
        c(paste("No PCA",round(mean(noPCA_noTCGA_ttest$auc),3)),paste("PCA Feature Selection", round(mean(PCA_noTCGA_ttest_3$auc),3)), paste("PCA Adjustment (3 PCs)", round(mean(PCA_noTCGA_ttest_3$auc_adj),3))), col=c("black", "red", "green"), lty = 1, lwd=2)      
 dev.off()
@@ -38,3 +41,30 @@ dev.off()
 saveRDS(noPCA_noTCGA_ttest, paste0(outDir,"/noPCA_noTCGA_RFcv.RDS"))
 saveRDS(PCA_noTCGA_ttest_3, paste0(outDir, "/PCA3_noTCGA_RFcv.RDS"))
 
+### Now let's look at what's happening when we do the PCA adjustment ###
+
+noPCA_import <- apply(noPCA_noTCGA_ttest$var_import, 1, mean, na.rm=T)
+PCA_import <- apply(PCA_noTCGA_ttest_3$var_import, 1, mean, na.rm=T)
+
+plot(noPCA_import, PCA_import, pch=NA_integer_, asp=1)
+text(noPCA_import, PCA_import, labels = names(noPCA_import))
+abline(0,1, col="red")
+
+change_impt <- PCA_import-noPCA_import
+
+num_PC<-3
+pca <- prcomp(samps[adeno==F,], center=T)
+eigen <- pca$rotation[,1:num_PC]
+# Get "adjusted" expression matrix #
+samps_cent <- t(apply(samps, 1, function(x) (x-pca$center)))
+orth_comp <- as.matrix(samps) - t(apply(as.matrix(samps_cent)%*%eigen%*%t(eigen),1,function(x) x+pca$center))
+boxplot(samps[,"TLE1"]~adeno, main="TLE1 - no PCA")
+boxplot(orth_comp[,"TLE1"]~adeno, main="TLE1 - PCA")
+boxplot(samps[,"OR51E2"]~adeno, main=" OR51E2 - no PCA")
+boxplot(orth_comp[,"OR51E2"]~adeno, main=" OR51E2- PCA")
+boxplot(samps[,"CH25H"]~adeno, main="TLE1 - no PCA")
+boxplot(orth_comp[,"CH25H"]~adeno, main="TLE1 - PCA")
+boxplot(samps[,"RP11-375D13.1"]~adeno, main="RP11-375D13.1 - no PCA")
+boxplot(orth_comp[,"RP11-375D13.1"]~adeno, main="RP11-375D13.1 - PCA")
+boxplot(samps[,"FILIP1"]~adeno, main="FILIP1 - no PCA")
+boxplot(orth_comp[,"FILIP1"]~adeno, main="FILIP1 - PCA")
